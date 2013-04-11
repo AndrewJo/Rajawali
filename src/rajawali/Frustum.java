658ccf60068785284d8e03ef6f6616ec00aab227
Jared Woolston <jwoolston@tenkiv.com>
2013-04-10 23:11:21 -0700

    Working on camera stuff.
    Signed-off-by: Jared Woolston <jwoolston@tenkiv.com>

--- src/rajawali/Frustum.java
+++ src/rajawali/Frustum.java
@@ -1,15 +1,17 @@
 package rajawali;
 
 import rajawali.bounds.BoundingBox;
-import rajawali.bounds.IBoundingVolume;
 import rajawali.math.Number3D;
 import rajawali.math.Plane;
 import rajawali.math.Plane.PlaneSide;
 import rajawali.primitives.Sphere;
+import android.opengl.Matrix;
+import android.util.Log;
 
 public class Frustum {
 	private Number3D[] mTmp = new Number3D[8];
 	protected Sphere mVisualSphere;
+	protected BoundingBox mBoundingBox;
 	protected float[] mTmpMatrix = new float[16];
 	protected static final Number3D[] mClipSpacePlanePoints = { 
 		new Number3D(-1, -1, -1), 
@@ -40,7 +42,8 @@ public class Frustum {
 
 		for(int i = 0; i < 8; i++) {
 			planePoints[i].setAllFrom(mClipSpacePlanePoints[i]);
-			planePoints[i].project(inverseProjectionView);                    
+			planePoints[i].project(inverseProjectionView);   
+			Log.v("Rajawali", "Point " + i + ": " + planePoints[i].toString());
 		}
 
 		planes[0].set(planePoints[1], planePoints[0], planePoints[2]);
@@ -49,6 +52,7 @@ public class Frustum {
 		planes[3].set(planePoints[5], planePoints[1], planePoints[6]);
 		planes[4].set(planePoints[2], planePoints[3], planePoints[6]);
 		planes[5].set(planePoints[4], planePoints[0], planePoints[1]);
+		setBounds();
 	}       
 
 
@@ -84,7 +88,40 @@ public class Frustum {
 		return true;
 	}
 	
+	/**
+	 * Sets the bounds of the frustum's BoundingBox.
+	 * Should be called internally whenever a change to
+	 * the frustum occurs.
+	 */
+	protected void setBounds() {
+		if (mBoundingBox == null) {
+			mBoundingBox = new BoundingBox();
+		}
+		Number3D min = new Number3D();
+		Number3D max = new Number3D();
+		min.setAllFrom(planePoints[0]);
+		min.x = planePoints[5].x;
+		min.y = planePoints[5].y;
+		max.setAllFrom(planePoints[7]);
+		Log.i("Rajawali", "Min/Max: " + min + "/" + max);
+		Matrix.setIdentityM(mTmpMatrix, 0);
+		mBoundingBox.setMin(min);
+		mBoundingBox.setMax(max);
+		//mBoundingBox.calculatePoints();
+		mBoundingBox.transform(mTmpMatrix);
+		Log.i("Rajawali", "Camera bounds: " + mBoundingBox);
+	}
+	
+	/**
+	 * Returns a BoundingBox representative of this frustum.
+	 * This will create the BoundingBox if necessary.
+	 * 
+	 * @return BoundingBox which contains this frustum.
+	 */
 	public BoundingBox getBoundingBox() {
-		return null;
+		if (mBoundingBox == null) {
+			setBounds();
+		} 
+		return mBoundingBox;
 	}
 }
