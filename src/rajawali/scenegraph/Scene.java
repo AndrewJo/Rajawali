9db7ab91b734b447dc723761219de0bf61e3705f
Jared Woolston <jwoolston@tenkiv.com>
2013-04-29 15:07:07 -0700

    Added Scene class.
    Signed-off-by: Jared Woolston <jwoolston@tenkiv.com>

---
+++ src/rajawali/scenegraph/Scene.java
@@ -0,0 +1,16 @@
+package rajawali.scenegraph;
+
+import rajawali.renderer.RajawaliRenderer;
+
+/**
+ * This is the container class for scenes in Rajawali.
+ * 
+ * It is intended that children, lights, cameras and animations
+ * will be added to this object and this object will be added
+ * to the {@link RajawaliRenderer} instance.
+ * 
+ * @author Jared Woolston (jwoolston@tenkiv.com)
+ */
+public class Scene {
+
+}
